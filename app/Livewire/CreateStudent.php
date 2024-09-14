ba12a71caa713a285cecbd5eff874cbe8dddb9a0
Daniel Shahzada <107118950+PS213073@users.noreply.github.com>
2024-09-14 20:59:59 +0200

    defining dedicated from object for create flow

--- app/Livewire/CreateStudent.php
+++ app/Livewire/CreateStudent.php
@@ -2,45 +2,28 @@
 
 namespace App\Livewire;
 
+use App\Livewire\Forms\CreateStudentForm;
 use App\Models\Classes;
-use App\Models\Section;
-use App\Models\Student;
 use Livewire\Attributes\Validate;
 use Livewire\Component;
 
 class CreateStudent extends Component
 {
-    #[Validate('required')]
-    public $name;
-
-    #[Validate('required|email|unique:students,email')]
-    public $email;
+    public CreateStudentForm $form;
 
     #[Validate('required')]
     public $class_id;
 
-    #[Validate('required')]
-    public $section_id;
-
-    public $sections = [];
-
     public function addStudent()
     {
-        $this->validate();
-
-        Student::create([
-            'name' => $this->name,
-            'email' => $this->email,
-            'class_id' => $this->class_id,
-            'section_id' => $this->section_id,
-        ]);
+        $this->form->storeStudent($this->class_id);
 
         return redirect(route('students.index'));
     }
 
     public function updatedClassId($classId)
     {
-        $this->sections = Section::where('class_id', $classId)->get();
+        $this->form->setSections($classId);
     }
 
     public function render()
